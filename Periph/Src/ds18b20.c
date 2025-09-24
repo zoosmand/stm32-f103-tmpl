4cbdb27898c626710eda254ac2692deb93269817
Dmitry Slobodchikov <d.slobodchikov@intraclear.com>
2025-09-24 14:08:03 +0300

    Refactor code stage #06

--- Periph/Src/ds18b20.c
+++ Periph/Src/ds18b20.c
@@ -12,8 +12,6 @@
 /* Global variables ----------------------------------------------------------*/
 
 /* Private variables ---------------------------------------------------------*/
-// static uint8_t spad[9];
-
 
 /* Private function prototypes -----------------------------------------------*/
 
@@ -28,6 +26,8 @@ static int dS18B20_ReadScratchpad(uint8_t*, uint8_t*);
 static void dS18B20_ErrorHandler(void);
 
 static int dS18B20_ConvertTemperature(uint8_t*);
+
+__STATIC_INLINE void dS18B20_WaitStatus(uint16_t);
 
 
 
@@ -73,19 +73,7 @@ static int dS18B20_Read(uint8_t len, uint8_t* buf, uint8_t reverse) {
 
 // -------------------------------------------------------------  
 static int dS18B20_ReadScratchpad(uint8_t* buf, uint8_t* addr) {
-  // if (OneWire_Reset()) return (1);
-  
-  // dS18B20_Command(MatchROM);
 
-  // // dS18B20_Write(8, addr);
-  // dS18B20_Command(ReadScratchpad);
-
-  // uint32_t __delay = sysCnt + 2;
-  // while (__delay >= sysCnt) {__asm volatile("nop");};
-  
-  // dS18B20_Read(9, buf, 0);
-  
-  // return (0);
   if (OneWire_MatchROM(addr)) return (1);
   dS18B20_Command(ReadScratchpad);
 
@@ -104,40 +92,6 @@ static int dS18B20_ReadScratchpad(uint8_t* buf, uint8_t* addr) {
 
 // -------------------------------------------------------------  
 static int dS18B20_ConvertTemperature(uint8_t* addr) {
-  
-  // if (OneWire_Reset()) return (1);
-
-
-  // if (OneWire_MatchROM(addr)) return 1;
-  // uint8_t pps = OneWire_ReadPowerSupply(addr);
-
-
-
-  // if (OneWire_MatchROM(addr)) return 1;
-  // OneWire_WriteByte(ConvertT);
-  
-  // dS18B20_Command(SkipROM);
-  // dS18B20_Command(ConvertT);
-  // // uint32_t __delay = sysCnt + 750;
-  // // while (__delay >= sysCnt) {__asm volatile("nop");};
-  // while(!OneWire_ReadBit()) {__asm volatile("nop");};
-  
-  // OneWireDevice_t* oneWireDevices = Get_OwDevices();
-  
-  // for (uint8_t i = 0; i < 2; i++) {
-    
-    
-  //   dS18B20_ReadScratchpad(oneWireDevices[i].spad, oneWireDevices[i].addr);
-  //   while(!OneWire_ReadBit());
-  // }
-  // uint32_t* t1 = (int32_t*)&oneWireDevices[0].spad;
-  // uint32_t* t2 = (int32_t*)&oneWireDevices[1].spad;
-  // printf("%d.%02d %d.%02d\n", 
-  //   (int8_t)((*t1 & 0x0000fff0) >> 4), (uint8_t)(((*t1 & 0x0000000f) * 100) >> 4),
-  //   (int8_t)((*t2 & 0x0000fff0) >> 4), (uint8_t)(((*t2 & 0x0000000f) * 100) >> 4)
-  // );
-  
-  // return (0);
 
   if (*addr) {
 
@@ -151,27 +105,35 @@ static int dS18B20_ConvertTemperature(uint8_t* addr) {
       PIN_H(OneWire_PORT, OneWire_PIN);
       
       uint32_t __delay = sysCnt + 750;
-      while (__delay >= sysCnt) {__asm volatile("nop");};
+      dS18B20_WaitStatus(3);
 
       PIN_L(OneWire_PORT, OneWire_PIN);
     } else {
-      /* TDOD implement timeout, handle independently */
-      while(!OneWire_ReadBit()) {__asm volatile("nop");};
+      dS18B20_WaitStatus(3);
     }
 
   } else {
+    if (OneWire_Reset()) return (1);
+
     dS18B20_Command(SkipROM);
-    while(!OneWire_ReadBit()) {__asm volatile("nop");};
-    
     dS18B20_Command(ConvertT);
-    while(!OneWire_ReadBit()) {__asm volatile("nop");};
-    
-    uint32_t __delay = sysCnt + 750;
-    while (__delay >= sysCnt) {__asm volatile("nop");};
+    dS18B20_WaitStatus(3);
   }
   
   return 0;
 
+}
+
+
+
+
+// -------------------------------------------------------------  
+__STATIC_INLINE void dS18B20_WaitStatus(uint16_t ms) {
+  uint32_t wait_threshold = sysCnt + ms;
+  while(!OneWire_ReadBit()) {
+    __asm volatile("nop");
+    if (sysCnt >= wait_threshold) return;
+  };
 }
 
 
@@ -191,15 +153,14 @@ int DS18B20_GetTemperatureMeasurment(void) {
 
   OneWireDevice_t* oneWireDevices = Get_OwDevices();
 
-  uint8_t addd = 0;
-
-  dS18B20_ConvertTemperature(&addd);
-
   for (uint8_t i = 0; i < 2; i++) {
+    dS18B20_ConvertTemperature(oneWireDevices[i].addr);
     
+    uint32_t __delay = sysCnt + 3;
+    while (__delay >= sysCnt) {__asm volatile("nop");};
     
     dS18B20_ReadScratchpad(oneWireDevices[i].spad, oneWireDevices[i].addr);
-    while(!OneWire_ReadBit());
+    while(!OneWire_ReadBit()) {__asm volatile("nop");};
   }
   uint32_t* t1 = (int32_t*)&oneWireDevices[0].spad;
   uint32_t* t2 = (int32_t*)&oneWireDevices[1].spad;
